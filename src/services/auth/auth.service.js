a5d3978e0cb21272001180c11397b38925d794cf
Vijay-Apple <vijayraj639726@gmail.com>
2026-07-06 17:38:44 +0000

    fix routes

--- src/services/auth/auth.service.js
+++ src/services/auth/auth.service.js
@@ -1,40 +1,28 @@
 import apiClient from "../api/apiClient";
-import axios from "axios";
 
-const API_URL = "/api/v1/auth";
+const API_URL = "/auth";
 
 export const registerUser = async (data) => {
-    const response = await axios.post(
-        `${API_URL}/register`,
-        data
-    );
-
+    const response = await apiClient.post(`${API_URL}/register`, data);
     return response.data;
 };
-export const loginUser = async (data) => {
-    const response = await axios.post(
-        `${API_URL}/login`,
-        data
-    );
 
+export const loginUser = async (data) => {
+    const response = await apiClient.post(`${API_URL}/login`, data);
     return response.data;
 };
 
 export const forgotPassword = async (email) => {
-    const response = await axios.post(
-        `${API_URL}/forgot-password`,
-        {
-            email,
-        }
-    );
-
+    const response = await apiClient.post(`${API_URL}/forgot-password`, {
+        email,
+    });
     return response.data;
 };
+
 export const resetPassword = async (token, data) => {
-    const response = await axios.post(
+    const response = await apiClient.post(
         `${API_URL}/reset-password/${token}`,
         data
     );
-
     return response.data;
 };
